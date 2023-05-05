6c8480de5ea916fb315c93a093652e1340d2cda9
melvinkj <melvinkentj@gmail.com>
2023-05-05 23:45:38 +0700

    doc:  update landing page

--- frontend/src/app/head.tsx
+++ frontend/src/app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
     return (
         <>
-            <title>Simple ChatGPT</title>
+            <title>GIGAChat</title>
             <meta content="width=devide-width, initial-scale=1" name="viewport"/>
             <link rel="icon" href="/favicon.ico"/>
         </>
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import { SunIcon } from '@heroicons/react/24/outline'
 function HomePage() {
   return (
     <div className='flex flex-col items-center justify-center h-screen px-2 text-white'>
-        <h1 className='text-5xl font-bold mb-20'>Simple ChatGPT</h1>
+        <h1 className='text-5xl font-bold mb-20'>Welcome to GIGAChat</h1>
 
         <div className='flex space-x-2 text-center'>
           <div>
@@ -11,13 +11,13 @@ function HomePage() {
               {/* Sun Icon */}
               <SunIcon className="h-6 w-6 text-white"/>
 
-              <h2>Examples</h2>
+              <h2>Capital City</h2>
             </div>
 
             <div className='space-y-2'>
-              <p className='infoText'>"Explain something to me"</p>
-              <p className='infoText'>"What is the difference between a dog and a cat"</p>
-              <p className='infoText'>"What is the color of the sun"</p>
+              <p className='infoText'>"Apa ibukota Indonesia"</p>
+              <p className='infoText'>"Apa ibukota Gambia"</p>
+              <p className='infoText'>"Apa ibukota India"</p>
             </div>
           </div>
           <div>
@@ -25,13 +25,13 @@ function HomePage() {
               {/* Sun Icon */}
               <SunIcon className="h-6 w-6 text-white" />
 
-              <h2>Examples</h2>
+              <h2>Counting</h2>
             </div>
 
             <div className="space-y-2">
-              <p className='infoText'>"Explain something to me"</p>
-              <p className='infoText'>"what is the difference between a dog and a cat"</p>
-              <p className='infoText'>"What is the color of the sun"</p>
+              <p className='infoText'>"Hitung 3*5*7-(1-3)"</p>
+              <p className='infoText'>"Hitung 5*4"</p>
+              <p className='infoText'>"Hari apa 23/04/2012"</p>
             </div>
           </div>
           <div>
@@ -39,13 +39,13 @@ function HomePage() {
               {/* Sun Icon */}
               <SunIcon className="h-6 w-6 text-white" />
 
-              <h2>Examples</h2>
+              <h2>Pick</h2>
             </div>
 
             <div className="space-y-2">
-              <p className='infoText'>"Explain something to me"</p>
-              <p className='infoText'>"what is the difference between a dog and a cat"</p>
-              <p className='infoText'>"What is the color of the sun"</p>
+              <p className='infoText'>"Pilih 2 dari 1-155"</p>
+              <p className='infoText'>"Pilih 1 dari bubur diaduk, ga diaduk"</p>
+              <p className='infoText'>"Pilih 1 dari a, b, c, d"</p>
             </div>
           </div>
 
