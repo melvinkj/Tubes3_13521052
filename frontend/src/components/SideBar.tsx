4de310e168d21715f0b0efcce867a58af09e37c9
yuujin-Q <13521074@mahasiswa.itb.ac.id>
2023-05-05 11:11:07 +0700

    fix:sidebar

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,26 +1,29 @@
 "use client";
 import NewChat from "./NewChat"
 import ChatRow from "./ChatRow";
-import { useEffect, useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import { db } from "@/lib/prisma";
 import Chat from "./Chat";
 
 function SideBar() {
-    // const { data:session } = useSession();
 
     const [messages, setMessages] = useState<any[]>([]);
 
-    useEffect(
+    useLayoutEffect(
         () => {
             const initMessages = async () => {
                 const response = await fetch('api/chats', {
                     method: "GET",
                 });
-                setMessages(await response.json())
+                console.log("P");
+                console.log(response);
+                const message = await response.json();
+                console.log(message)
+                setMessages(message)
             }
 
             initMessages();
-        }
+        }, [setMessages]
     )
     // const chats = await db.chats.findMany({
     //     orderBy: { createdAt: "asc" },
@@ -43,23 +46,10 @@ function SideBar() {
                 <div>
                     {/* Map through the chat rows */}
                     
-                    {/* {chats?.docs.map(chat => (
-                        <ChatRow key={chat.id} id={chat.id} />
-                    ))} */}
-                    {/* {messages.map(chat => (<ChatRow chatId={chat.chatId}/>))} */}
-                    <ChatRow chatId={1} topic={"ayam"}/>
-                    {/* chore: update chat view from db */}
+                    {messages.map(chat => (<ChatRow chatId={chat.chatId} topic={chat.topic}/>))}
                 </div>
             </div>           
         </div>
-        {/* {session && (
-                <img 
-                onClick={()=> signOut()}
-                src={session.user?.image!}
-                // chore: placeholder for empty image
-                alt="Profile pic" 
-                className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50" />   
-        )} */}
     </div>
   )
 }
